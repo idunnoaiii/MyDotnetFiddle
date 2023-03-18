abedf32564b85c78119fdc1f3edf0af0653e465d
thien.nguyen <idunno.aiii@gmail.com>
2023-03-18 17:10:56 +0700

    add some init project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var client = new  HttpClient();
+
+client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+var response = await  client.GetStringAsync("/posts/1");
+
+Console.WriteLine(response);
---
+++ Helper/Helper.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Helper;
+
+public static class Helper
+{
+    public static string Dump<T>(this T data)
+    {
+        var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        Console.WriteLine(json);
+        return json;
+    }
+    
+    
+}
